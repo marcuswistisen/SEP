0e65d1047521cf0c4bb441b8d08f67f0d662ef6b
Marcus Wistisen <marcus.wistisen@gmail.com>
2014-10-23 17:03:07 +1100

    COWABUNGA DUDE

---
+++ Database/ApplicationDB.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 23, 2014 at 06:00 AM
+-- Server version: 5.5.39-MariaDB
+-- PHP Version: 5.5.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ApplicationDB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Application`
+--
+
+CREATE TABLE IF NOT EXISTS `Application` (
+`application_id` int(10) NOT NULL,
+  `uts_id` int(10) NOT NULL,
+  `university_id` int(10) NOT NULL,
+  `justification_id` int(10) NOT NULL,
+  `cost_id` int(10) NOT NULL,
+  `conferance_id` int(10) NOT NULL,
+  `travel_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Conferance`
+--
+
+CREATE TABLE IF NOT EXISTS `Conferance` (
+`conferance_id` int(10) NOT NULL,
+  `conference_url` varchar(50) NOT NULL,
+  `conference_name` varchar(50) NOT NULL,
+  `conference_start` date NOT NULL,
+  `conference_end` date NOT NULL,
+  `conference_city` varchar(50) NOT NULL,
+  `conference_country` varchar(50) NOT NULL,
+  `conferance_quality` int(5) NOT NULL,
+  `special_invitation` tinyint(1) NOT NULL,
+  `pep_arrangement` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Cost`
+--
+
+CREATE TABLE IF NOT EXISTS `Cost` (
+`cost_id` int(10) NOT NULL,
+  `air_fares` double NOT NULL,
+  `meal_cost` double NOT NULL,
+  `accomondation_cost` double NOT NULL,
+  `conferance_cost` double NOT NULL,
+  `local_fares_cost` double NOT NULL,
+  `car_milage_cost` double NOT NULL,
+  `other_cost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Justification`
+--
+
+CREATE TABLE IF NOT EXISTS `Justification` (
+`justification_id` int(11) NOT NULL,
+  `paper_title` varchar(50) NOT NULL,
+  `evidence` text NOT NULL,
+  `journal_accepted` tinyint(1) NOT NULL,
+  `peer_review_happend` tinyint(1) NOT NULL,
+  `journal_declared` tinyint(1) NOT NULL,
+  `peer_review_url` varchar(50) NOT NULL,
+  `copy_paper_url` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Travel`
+--
+
+CREATE TABLE IF NOT EXISTS `Travel` (
+`travel_id` int(10) NOT NULL,
+  `travel_start` date NOT NULL,
+  `travel_end` date NOT NULL,
+  `travel_loc` enum('Domestic','International','','') NOT NULL,
+  `travel_justification` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `University`
+--
+
+CREATE TABLE IF NOT EXISTS `University` (
+`university_id` int(10) NOT NULL,
+  `supervisor` varchar(50) NOT NULL,
+  `research_student` tinyint(1) NOT NULL,
+  `research_grant` tinyint(1) NOT NULL,
+  `research_stregth` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Application`
+--
+ALTER TABLE `Application`
+ ADD PRIMARY KEY (`application_id`), ADD UNIQUE KEY `uts_id_2` (`uts_id`), ADD UNIQUE KEY `university_id` (`university_id`), ADD KEY `uts_id` (`uts_id`,`university_id`,`justification_id`), ADD KEY `cost_id` (`cost_id`,`conferance_id`,`travel_id`), ADD KEY `justification_id` (`justification_id`), ADD KEY `cost_id_2` (`cost_id`), ADD KEY `conferance_id` (`conferance_id`), ADD KEY `travel_id` (`travel_id`);
+
+--
+-- Indexes for table `Conferance`
+--
+ALTER TABLE `Conferance`
+ ADD PRIMARY KEY (`conferance_id`);
+
+--
+-- Indexes for table `Cost`
+--
+ALTER TABLE `Cost`
+ ADD PRIMARY KEY (`cost_id`);
+
+--
+-- Indexes for table `Justification`
+--
+ALTER TABLE `Justification`
+ ADD PRIMARY KEY (`justification_id`);
+
+--
+-- Indexes for table `Travel`
+--
+ALTER TABLE `Travel`
+ ADD PRIMARY KEY (`travel_id`);
+
+--
+-- Indexes for table `University`
+--
+ALTER TABLE `University`
+ ADD PRIMARY KEY (`university_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Application`
+--
+ALTER TABLE `Application`
+MODIFY `application_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Conferance`
+--
+ALTER TABLE `Conferance`
+MODIFY `conferance_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Cost`
+--
+ALTER TABLE `Cost`
+MODIFY `cost_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Justification`
+--
+ALTER TABLE `Justification`
+MODIFY `justification_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Travel`
+--
+ALTER TABLE `Travel`
+MODIFY `travel_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `University`
+--
+ALTER TABLE `University`
+MODIFY `university_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Application`
+--
+ALTER TABLE `Application`
+ADD CONSTRAINT `Application_ibfk_6` FOREIGN KEY (`travel_id`) REFERENCES `Travel` (`travel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_1` FOREIGN KEY (`uts_id`) REFERENCES `LoginDB`.`User` (`uts_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_2` FOREIGN KEY (`university_id`) REFERENCES `University` (`university_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_3` FOREIGN KEY (`justification_id`) REFERENCES `Justification` (`justification_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_4` FOREIGN KEY (`cost_id`) REFERENCES `Cost` (`cost_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_5` FOREIGN KEY (`conferance_id`) REFERENCES `Conferance` (`conferance_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ application.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Online Travel Funding</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+<script type="text/javascript" src="js/view.js"></script>
+<script type="text/javascript" src="js/calendar.js"></script>
+</head>
+<div id="application" class="hidden">
+<body id="application_body">
+	
+	<img id="top" src="images/top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>Online Travel Funding</a></h1>
+		<form id="form_919735" class="appnitro"  method="post" action="">
+					<div class="form_description">
+			<h2>Online Travel Funding</h2>
+			<p></p>
+		</div>						
+			<ul >
+			
+					<li id="li_1" >
+		<label class="description" for="element_1">First Name </label>
+		<div>
+			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_2" >
+		<label class="description" for="element_2">Preferred First Name  </label>
+		<div>
+			<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_3" >
+		<label class="description" for="element_3">Email </label>
+		<div>
+			<input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_4" >
+		<label class="description" for="element_4">Phone </label>
+		<div>
+			<input id="element_4" name="element_4" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_5" >
+		<label class="description" for="element_5">School Centre </label>
+		<div>
+			<input id="element_5" name="element_5" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_6" >
+		<label class="description" for="element_6">UTS ID </label>
+		<div>
+			<input id="element_6" name="element_6" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_7" >
+		<label class="description" for="element_7">Supervisor </label>
+		<div>
+			<input id="element_7" name="element_7" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_28" >
+		<label class="description" for="element_28"> </label>
+		<span>
+			<input id="element_28_1" name="element_28_1" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_28_1">I am a research student</label>
+<input id="element_28_2" name="element_28_2" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_28_2">I have research grant</label>
+<input id="element_28_3" name="element_28_3" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_28_3">I have research strength</label>
+
+		</span> 
+		</li>		<li id="li_8" >
+		<label class="description" for="element_8">Paper Title </label>
+		<div>
+			<input id="element_8" name="element_8" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_9" >
+		<label class="description" for="element_9">Evidence </label>
+		<div>
+			<textarea id="element_9" name="element_9" class="element textarea medium"></textarea> 
+		</div> 
+		</li>		<li id="li_29" >
+		<label class="description" for="element_29"> </label>
+		<span>
+			<input id="element_29_1" name="element_29_1" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_29_1">Journal Accepted</label>
+<input id="element_29_2" name="element_29_2" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_29_2">Peer Review Happend</label>
+<input id="element_29_3" name="element_29_3" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_29_3">Journal Declared</label>
+
+		</span> 
+		</li>		<li id="li_10" >
+		<label class="description" for="element_10">Peer Review URL </label>
+		<div>
+			<input id="element_10" name="element_10" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_11" >
+		<label class="description" for="element_11">Copy Paper URL </label>
+		<div>
+			<input id="element_11" name="element_11" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_12" >
+		<label class="description" for="element_12">Conference URL </label>
+		<div>
+			<input id="element_12" name="element_12" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_13" >
+		<label class="description" for="element_13">Conference Name </label>
+		<div>
+			<input id="element_13" name="element_13" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_14" >
+		<label class="description" for="element_14">Conferance Start </label>
+		<span>
+			<input id="element_14_1" name="element_14_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_14_1">MM</label>
+		</span>
+		<span>
+			<input id="element_14_2" name="element_14_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_14_2">DD</label>
+		</span>
+		<span>
+	 		<input id="element_14_3" name="element_14_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_14_3">YYYY</label>
+		</span>
+	
+		<span id="calendar_14">
+			<img id="cal_img_14" class="datepicker" src="images/calendar.gif" alt="Pick a date.">
+		</span>
+		<script type="text/javascript">
+			Calendar.setup({
+			inputField	 : "element_14_3",
+			baseField    : "element_14",
+			displayArea  : "calendar_14",
+			button		 : "cal_img_14",
+			ifFormat	 : "%B %e, %Y",
+			onSelect	 : selectDate
+			});
+		</script>
+		 
+		</li>		<li id="li_15" >
+		<label class="description" for="element_15">Conference  End </label>
+		<span>
+			<input id="element_15_1" name="element_15_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_15_1">MM</label>
+		</span>
+		<span>
+			<input id="element_15_2" name="element_15_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_15_2">DD</label>
+		</span>
+		<span>
+	 		<input id="element_15_3" name="element_15_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_15_3">YYYY</label>
+		</span>
+	
+		<span id="calendar_15">
+			<img id="cal_img_15" class="datepicker" src="images/calendar.gif" alt="Pick a date.">
+		</span>
+		<script type="text/javascript">
+			Calendar.setup({
+			inputField	 : "element_15_3",
+			baseField    : "element_15",
+			displayArea  : "calendar_15",
+			button		 : "cal_img_15",
+			ifFormat	 : "%B %e, %Y",
+			onSelect	 : selectDate
+			});
+		</script>
+		 
+		</li>		<li id="li_16" >
+		<label class="description" for="element_16">Conference Country </label>
+		<div>
+			<input id="element_16" name="element_16" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_17" >
+		<label class="description" for="element_17">Conference Quality </label>
+		<div>
+			<input id="element_17" name="element_17" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_30" >
+		<label class="description" for="element_30"> </label>
+		<span>
+			<input id="element_30_1" name="element_30_1" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_30_1">Special Invitation</label>
+<input id="element_30_2" name="element_30_2" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_30_2">PEP Arrangement</label>
+
+		</span> 
+		</li>		<li id="li_18" >
+		<label class="description" for="element_18">Travel Start </label>
+		<span>
+			<input id="element_18_1" name="element_18_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_18_1">MM</label>
+		</span>
+		<span>
+			<input id="element_18_2" name="element_18_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_18_2">DD</label>
+		</span>
+		<span>
+	 		<input id="element_18_3" name="element_18_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_18_3">YYYY</label>
+		</span>
+	
+		<span id="calendar_18">
+			<img id="cal_img_18" class="datepicker" src="images/calendar.gif" alt="Pick a date.">
+		</span>
+		<script type="text/javascript">
+			Calendar.setup({
+			inputField	 : "element_18_3",
+			baseField    : "element_18",
+			displayArea  : "calendar_18",
+			button		 : "cal_img_18",
+			ifFormat	 : "%B %e, %Y",
+			onSelect	 : selectDate
+			});
+		</script>
+		 
+		</li>		<li id="li_19" >
+		<label class="description" for="element_19">Travel End </label>
+		<span>
+			<input id="element_19_1" name="element_19_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_19_1">MM</label>
+		</span>
+		<span>
+			<input id="element_19_2" name="element_19_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_19_2">DD</label>
+		</span>
+		<span>
+	 		<input id="element_19_3" name="element_19_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_19_3">YYYY</label>
+		</span>
+	
+		<span id="calendar_19">
+			<img id="cal_imif_19" class="datepicker" src="images/calendar.gif" alt="Pick a date.">
+		</span>
+		<script type="text/javascript">
+			Calendar.setup({
+			inputField	 : "element_19_3",
+			baseField    : "element_19",
+			displayArea  : "calendar_19",
+			button		 : "cal_img_19",
+			ifFormat	 : "%B %e, %Y",
+			onSelect	 : selectDate
+			});
+		</script>
+		 
+		</li>		<li id="li_31" >
+		<label class="description" for="element_31">Travel Location </label>
+		<div>
+		<select class="element select medium" id="element_31" name="element_31"> 
+			<option value="" selected="selected"></option>
+<option value="1" >Domestic</option>
+<option value="2" >International</option>
+
+		</select>
+		</div> 
+		</li>		<li id="li_20" >
+		<label class="description" for="element_20">Travel Justification </label>
+		<div>
+			<textarea id="element_20" name="element_20" class="element textarea medium"></textarea> 
+		</div> 
+		</li>		<li id="li_21" >
+		<label class="description" for="element_21">Air Fares Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_21_1" name="element_21_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_21_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_21_2" name="element_21_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_21_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_22" >
+		<label class="description" for="element_22">Meals Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_22_1" name="element_22_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_22_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_22_2" name="element_22_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_22_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_23" >
+		<label class="description" for="element_23">Accomondation Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_23_1" name="element_23_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_23_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_23_2" name="element_23_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_23_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_24" >
+		<label class="description" for="element_24">Conferance Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_24_1" name="element_24_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_24_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_24_2" name="element_24_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_24_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_25" >
+		<label class="description" for="element_25">Local Fares Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_25_1" name="element_25_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_25_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_25_2" name="element_25_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_25_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_26" >
+		<label class="description" for="element_26">Car Mileage Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_26_1" name="element_26_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_26_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_26_2" name="element_26_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_26_2">Cents</label>
+		</span>
+		 
+		</li>		<li id="li_27" >
+		<label class="description" for="element_27">Other Cost </label>
+		<span class="symbol">$</span>
+		<span>
+			<input id="element_27_1" name="element_27_1" class="element text currency" size="10" value="" type="text" /> .		
+			<label for="element_27_1">Dollars</label>
+		</span>
+		<span>
+			<input id="element_27_2" name="element_27_2" class="element text" size="2" maxlength="2" value="" type="text" />
+			<label for="element_27_2">Cents</label>
+		</span>
+		 
+		</li>
+			
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="919735" />
+			    
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
+                        <input id="Cancel" class="button_text" type="button" onclick="ShowApplication()" name="cancel" value="Cancel" />
+		</li>
+			</ul>
+		</form>	
+		<div id="footer">
+		</div>
+	</div>
+	<img id="bottom" src="images/bottom.png" alt="">
+	</body>
+</div>
+</html>
