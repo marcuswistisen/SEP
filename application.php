388755db2d313bcae4eb9927d1c8f854969ad33b
Marcus Wistisen <marcus.wistisen@gmail.com>
2014-10-23 17:04:49 +1100

    COWABUNGA DUDE

--- application.php
+++ application.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Online Travel Funding</title>
+<title>Online Travel Funding Application</title>
 <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
 <script type="text/javascript" src="js/view.js"></script>
 <script type="text/javascript" src="js/calendar.js"></script>
