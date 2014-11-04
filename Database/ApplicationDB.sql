44a0692d2b18d6a50d87920a06683da05cdb1edb
Marcus Wistisen <marcus.wistisen@gmail.com>
2014-11-04 15:01:52 +1100

    DONE

--- Database/ApplicationDB.sql
+++ Database/ApplicationDB.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.2.10.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 23, 2014 at 06:00 AM
+-- Generation Time: Nov 04, 2014 at 03:01 PM
 -- Server version: 5.5.39-MariaDB
--- PHP Version: 5.5.17
+-- PHP Version: 5.5.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `Application` (
   `cost_id` int(10) NOT NULL,
   `conferance_id` int(10) NOT NULL,
   `travel_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,12 +48,11 @@ CREATE TABLE IF NOT EXISTS `Conferance` (
   `conference_name` varchar(50) NOT NULL,
   `conference_start` date NOT NULL,
   `conference_end` date NOT NULL,
-  `conference_city` varchar(50) NOT NULL,
   `conference_country` varchar(50) NOT NULL,
   `conferance_quality` int(5) NOT NULL,
   `special_invitation` tinyint(1) NOT NULL,
   `pep_arrangement` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -69,8 +68,8 @@ CREATE TABLE IF NOT EXISTS `Cost` (
   `conferance_cost` double NOT NULL,
   `local_fares_cost` double NOT NULL,
   `car_milage_cost` double NOT NULL,
-  `other_cost` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `other_cost` double NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -87,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `Justification` (
   `journal_declared` tinyint(1) NOT NULL,
   `peer_review_url` varchar(50) NOT NULL,
   `copy_paper_url` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -101,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `Travel` (
   `travel_end` date NOT NULL,
   `travel_loc` enum('Domestic','International','','') NOT NULL,
   `travel_justification` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -115,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `University` (
   `research_student` tinyint(1) NOT NULL,
   `research_grant` tinyint(1) NOT NULL,
   `research_stregth` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -165,32 +164,32 @@ ALTER TABLE `University`
 -- AUTO_INCREMENT for table `Application`
 --
 ALTER TABLE `Application`
-MODIFY `application_id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `application_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `Conferance`
 --
 ALTER TABLE `Conferance`
-MODIFY `conferance_id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `conferance_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `Cost`
 --
 ALTER TABLE `Cost`
-MODIFY `cost_id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `cost_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `Justification`
 --
 ALTER TABLE `Justification`
-MODIFY `justification_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `justification_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `Travel`
 --
 ALTER TABLE `Travel`
-MODIFY `travel_id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `travel_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `University`
 --
 ALTER TABLE `University`
-MODIFY `university_id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `university_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -199,12 +198,12 @@ MODIFY `university_id` int(10) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `Application`
 --
 ALTER TABLE `Application`
-ADD CONSTRAINT `Application_ibfk_6` FOREIGN KEY (`travel_id`) REFERENCES `Travel` (`travel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `Application_ibfk_1` FOREIGN KEY (`uts_id`) REFERENCES `LoginDB`.`User` (`uts_id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `Application_ibfk_2` FOREIGN KEY (`university_id`) REFERENCES `University` (`university_id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `Application_ibfk_3` FOREIGN KEY (`justification_id`) REFERENCES `Justification` (`justification_id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `Application_ibfk_4` FOREIGN KEY (`cost_id`) REFERENCES `Cost` (`cost_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `Application_ibfk_5` FOREIGN KEY (`conferance_id`) REFERENCES `Conferance` (`conferance_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `Application_ibfk_5` FOREIGN KEY (`conferance_id`) REFERENCES `Conferance` (`conferance_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Application_ibfk_6` FOREIGN KEY (`travel_id`) REFERENCES `Travel` (`travel_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
